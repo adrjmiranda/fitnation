5719a7f3f8fbbcb6b4331f36b1d4f8eb727cce13
Adriano Miranda <adrjmiranda@gmail.com>
2025-02-13 14:39:03 -0300

    style: created navbar style

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("console.log(\"It's working!\");\n\n\n//# sourceURL=webpack://fitnation/./src/index.ts?");
+eval("/**\n * Toggle Menu\n */\nvar navbarMenu = document.querySelector('.navbar-menu');\nvar navbarLinks = document.querySelectorAll('.navbar-link');\nvar navbarContent = document.querySelector('.navbar-content');\nvar toggleMenuButton = document.querySelector('#toggle-menu');\nvar showNavbar = 'show';\nvar changeToggleMenuButtonIcons = function () {\n    var icons = document.querySelectorAll('#toggle-menu i');\n    icons.length && icons.forEach(function (icon) { return icon.classList.toggle('hidden'); });\n};\nvar changeMenuView = function () {\n    changeToggleMenuButtonIcons();\n    navbarMenu === null || navbarMenu === void 0 ? void 0 : navbarMenu.classList.toggle('hidden');\n    navbarContent === null || navbarContent === void 0 ? void 0 : navbarContent.classList.toggle('change-color');\n};\nif (toggleMenuButton && navbarLinks.length) {\n    toggleMenuButton.addEventListener('click', function () {\n        changeMenuView();\n    });\n    navbarLinks.forEach(function (link) {\n        return link.addEventListener('click', function () {\n            changeMenuView();\n        });\n    });\n}\n\n\n//# sourceURL=webpack://fitnation/./src/index.ts?");
 
 /***/ })
 
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,42 @@
-console.log("It's working!");
+/**
+ * Toggle Menu
+ */
+const navbarMenu = document.querySelector('.navbar-menu') as HTMLUListElement;
+const navbarLinks = document.querySelectorAll(
+	'.navbar-link'
+) as NodeListOf<HTMLAnchorElement>;
+const navbarContent = document.querySelector(
+	'.navbar-content'
+) as HTMLDivElement;
+
+const toggleMenuButton = document.querySelector(
+	'#toggle-menu'
+) as HTMLButtonElement;
+
+const showNavbar: string = 'show';
+
+const changeToggleMenuButtonIcons = (): void => {
+	const icons = document.querySelectorAll(
+		'#toggle-menu i'
+	) as NodeListOf<HTMLElement>;
+
+	icons.length && icons.forEach((icon) => icon.classList.toggle('hidden'));
+};
+
+const changeMenuView = (): void => {
+	changeToggleMenuButtonIcons();
+	navbarMenu?.classList.toggle('hidden');
+	navbarContent?.classList.toggle('change-color');
+};
+
+if (toggleMenuButton && navbarLinks.length) {
+	toggleMenuButton.addEventListener('click', () => {
+		changeMenuView();
+	});
+
+	navbarLinks.forEach((link) =>
+		link.addEventListener('click', () => {
+			changeMenuView();
+		})
+	);
+}
