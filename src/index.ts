f2d346da2760364c80d434d418c1e55c4b5e30e4
Adriano Miranda <adrjmiranda@gmail.com>
2025-02-13 16:41:57 -0300

    style: created style for home banners

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+// General
+const getCSSVariableValue = (variable: string): string => {
+	const rootStyles: CSSStyleDeclaration = getComputedStyle(
+		document.documentElement
+	);
+
+	return rootStyles.getPropertyValue(variable).trim();
+};
+
 /**
  * Toggle Menu
  */
@@ -40,3 +49,50 @@ if (toggleMenuButton && navbarLinks.length) {
 		})
 	);
 }
+
+/**
+ * Banner Animation
+ */
+const bannerItems = document.querySelectorAll(
+	'.banner-item'
+) as NodeListOf<HTMLDivElement>;
+const bannerTexts = document.querySelectorAll(
+	'.banner-text'
+) as NodeListOf<HTMLDivElement>;
+
+const bannerItemsNumber: number = bannerItems?.length ?? 0;
+
+const bannerAnimationTime: string = getCSSVariableValue(
+	'--banner-animation-time'
+);
+
+const bannerTime = parseFloat(bannerAnimationTime.replace('s', '')) * 1000;
+const fadeBannerDelay = 200;
+
+let active = 0;
+
+const classFadeBanner = 'fade-banner';
+const classFadeBannerText = 'fade-banner-text';
+
+const clearFadeBanner = (): void => {
+	bannerItems?.forEach((item) => item.classList.remove(classFadeBanner));
+	bannerTexts?.forEach((text) => text.classList.remove(classFadeBannerText));
+};
+
+bannerItems[active]?.classList.add(classFadeBanner);
+bannerTexts[active]?.classList.add(classFadeBannerText);
+const bannerInterval = setInterval(() => {
+	const next: number = active + 1;
+	active = next >= bannerItemsNumber ? 0 : next;
+
+	clearFadeBanner();
+
+	setTimeout(() => {
+		bannerItems[active]?.classList.add(classFadeBanner);
+		bannerTexts[active]?.classList.add(classFadeBannerText);
+	}, fadeBannerDelay);
+}, bannerTime);
+
+window.addEventListener('beforeunload', () => {
+	clearInterval(bannerInterval);
+});
